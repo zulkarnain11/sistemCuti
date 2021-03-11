6ff3368bd3efa808258faed3a63ada52eb159345
root <root@kali>
2021-03-11 18:51:44 +0800

    first commit

---
+++ adminlogin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="css/style.css"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+      <div class="about">
+        <img src="image/logoktd.png" />
+        <h1>Online Leave Management System</h1>
+      </div>
+      <div class="log-in">
+        <form action="#" method="POST">
+            <h1>LOGIN</h1>
+                           
+            <img src="icon/user.svg" width="40px"/>
+            <input type="text" name="username" placeholder="Username" autocomplete="off"/><br/>
+            
+            <img src="icon/padlock.svg" width="40px"/>
+            <input type="password" name="pwd" placeholder="Password"/>
+            
+
+            <input type="submit" name="submit" value="Login"/>
+            <p><a href="login.php">Login as User ..</p>
+        </form>
+      </div>
+    </div>
+    <div class="footer">
+
+    </div>
+</body>
+</html>
